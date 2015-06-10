5aa6a05aa450e12b7e36610b2d0c00cc3b7143ba
Anthony <anthonygreen274@icloud.com>
2015-06-10 16:56:17 -0700

    Refactored Code

--- AWPopOutMenu.swift
+++ AWPopOutMenu.swift
@@ -46,41 +46,37 @@ class AWPopOutMenu: UIView {
     
     func setupView(){
         menuPressed = false;
-        
+
         // Start buttons below menu button and extend them out to their spots
-        leftMenuButton.frame = CGRectMake(0, 0, 50, 50)
+        leftMenuButton.frame = CGRectMake(0, 0, self.frame.size.height, self.frame.size.height)
         leftMenuButton.center = CGPointMake(self.frame.size.width/2, self.frame.size.height/2)
-        leftMenuButton.setTitle("L", forState: UIControlState.Normal)
-        leftMenuButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        leftMenuButton.setImage(UIImage(named: "Businessman Filled-50"), forState: .Normal)
         leftMenuButton.addTarget(self, action: "leftMenuButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
         leftMenuButton.hidden = true
         self.addSubview(leftMenuButton)
         
-        rightMenuButton.frame = CGRectMake(0, 0, 50, 50)
+        rightMenuButton.frame = CGRectMake(0, 0, self.frame.size.height, self.frame.size.height)
         rightMenuButton.center = CGPointMake(self.frame.size.width/2, self.frame.size.height/2)
-        rightMenuButton.setTitle("R", forState: UIControlState.Normal)
-        rightMenuButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        rightMenuButton.setImage(UIImage(named: "Horse-50"), forState: .Normal)
         rightMenuButton.addTarget(self, action: "rightMenuButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
         rightMenuButton.hidden = true
         self.addSubview(rightMenuButton)
         
-        outerLeftMenuButton.frame = CGRectMake(0, 0, 50, 50)
+        outerLeftMenuButton.frame = CGRectMake(0, 0, self.frame.size.height, self.frame.size.height)
         outerLeftMenuButton.center = CGPointMake(self.frame.size.width/2, self.frame.size.height/2)
-        outerLeftMenuButton.setTitle("OL", forState: UIControlState.Normal)
-        outerLeftMenuButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        outerLeftMenuButton.setImage(UIImage(named: "Mac Client-50"), forState: .Normal)
         outerLeftMenuButton.addTarget(self, action: "outerLeftMenuButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
         outerLeftMenuButton.hidden = true
         self.addSubview(outerLeftMenuButton)
         
-        outerRightMenuButton.frame = CGRectMake(0, 0, 50, 50)
+        outerRightMenuButton.frame = CGRectMake(0, 0, self.frame.size.height, self.frame.size.height)
         outerRightMenuButton.center = CGPointMake(self.frame.size.width/2, self.frame.size.height/2)
-        outerRightMenuButton.setTitle("OR", forState: UIControlState.Normal)
-        outerRightMenuButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        outerRightMenuButton.setImage(UIImage(named: "Soundcloud-50"), forState: .Normal)
         outerRightMenuButton.addTarget(self, action: "outerRightMenuButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
         outerRightMenuButton.hidden = true
         self.addSubview(outerRightMenuButton)
         
-        menuButton.frame = CGRectMake(0, 0, 50, 50)
+        menuButton.frame = CGRectMake(0, 0, self.frame.size.height, self.frame.size.height)
         menuButton.center = CGPointMake(self.frame.size.width/2, self.frame.size.height/2)
         menuButton.setTitle("+", forState: UIControlState.Normal)
         menuButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
@@ -93,7 +89,7 @@ class AWPopOutMenu: UIView {
     func menuButtonPressed(sender: UIButton){
         UIView.animateWithDuration(0.3, animations: { () -> Void in
             if (!self.menuPressed){
-                self.menuButton.setTitle("x", forState: UIControlState.Normal)
+                self.mainMenuRotation(self.menuButton)
                 
                 self.frame = CGRectMake(10, self.frame.origin.y, self.screenSize.width-20, self.frame.size.height)
                 
@@ -103,11 +99,6 @@ class AWPopOutMenu: UIView {
                 self.outerLeftMenuButton.center = CGPointMake(self.frame.size.width/2-120, self.frame.size.height/2)
                 self.outerRightMenuButton.center = CGPointMake(self.frame.size.width/2+120, self.frame.size.height/2)
                 
-                self.leftMenuButton.hidden = false
-                self.rightMenuButton.hidden = false
-                self.outerLeftMenuButton.hidden = false
-                self.outerRightMenuButton.hidden = false
-                
                 self.rotationAnimation(self.leftMenuButton)
                 self.rotationAnimation(self.rightMenuButton)
                 self.rotationAnimation(self.outerLeftMenuButton)
@@ -116,21 +107,15 @@ class AWPopOutMenu: UIView {
                 self.menuPressed = true
             }
             else{
+                self.mainMenuRotation(self.menuButton)
+                
                 self.rotationAnimation(self.leftMenuButton)
                 self.rotationAnimation(self.rightMenuButton)
                 self.rotationAnimation(self.outerLeftMenuButton)
                 self.rotationAnimation(self.outerRightMenuButton)
                 
-                self.leftMenuButton.hidden = true
-                self.rightMenuButton.hidden = true
-                self.outerLeftMenuButton.hidden = true
-                self.outerRightMenuButton.hidden = true
-                
                 self.frame = CGRectMake(self.screenSize.width/2-25, self.frame.origin.y, 50, self.frame.size.height)
-                
                 self.menuButton.center = CGPointMake(self.frame.size.width/2, self.frame.size.height/2)
-                
-                self.menuButton.setTitle("+", forState: UIControlState.Normal)
                 
                 self.menuPressed = false
             }
@@ -158,32 +143,71 @@ class AWPopOutMenu: UIView {
     
     // MARK: Button Animations
     
-    func rotationAnimation(sender: UIButton){
-        let fullRotation = CGFloat(M_PI * 2)
+    func mainMenuRotation(sender: UIButton){
+        var rot = CGFloat(M_PI_4 * 7.0)
+        
+        if (menuPressed){
+            rot = CGFloat(M_PI_4 * 2.0)
+        }
         
         UIView.animateKeyframesWithDuration(0.5, delay: 0.0, options: UIViewKeyframeAnimationOptions.CalculationModePaced, animations: { () -> Void in
             UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0, animations: {
-                sender.transform = CGAffineTransformMakeRotation(1/5 * fullRotation)
+                sender.transform = CGAffineTransformMakeRotation(1/5 * rot)
             })
             
             UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0, animations: {
-                sender.transform = CGAffineTransformMakeRotation(2/5 * fullRotation)
+                sender.transform = CGAffineTransformMakeRotation(2/5 * rot)
             })
             
             UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0, animations: {
-                sender.transform = CGAffineTransformMakeRotation(3/5 * fullRotation)
+                sender.transform = CGAffineTransformMakeRotation(3/5 * rot)
             })
             
             UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0, animations: {
-                sender.transform = CGAffineTransformMakeRotation(4/5 * fullRotation)
+                sender.transform = CGAffineTransformMakeRotation(4/5 * rot)
             })
             
             UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0, animations: {
-                sender.transform = CGAffineTransformMakeRotation(5/5 * fullRotation)
+                sender.transform = CGAffineTransformMakeRotation(5/5 * rot)
             })
-        }) { (menuPressed) -> Void in
-//            sender.hidden = false
-        }
+        }, completion: nil)
     }
-
+    
+    func rotationAnimation(sender: UIButton){
+        var rotation = CGFloat(M_PI * 2)
+        
+        if (!menuPressed){
+            sender.hidden = false
+        }
+        else{
+            let delayTime = dispatch_time(DISPATCH_TIME_NOW,
+                Int64(0.15 * Double(NSEC_PER_SEC)))
+            dispatch_after(delayTime, dispatch_get_main_queue()) {
+                sender.hidden = true;
+            }
+            rotation = -CGFloat(M_PI * 2)
+        }
+        
+        UIView.animateKeyframesWithDuration(0.5, delay: 0.0, options: UIViewKeyframeAnimationOptions.CalculationModePaced, animations: { () -> Void in
+            UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0, animations: {
+                sender.transform = CGAffineTransformMakeRotation(1/5 * rotation)
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0, animations: {
+                sender.transform = CGAffineTransformMakeRotation(2/5 * rotation)
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0, animations: {
+                sender.transform = CGAffineTransformMakeRotation(3/5 * rotation)
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0, animations: {
+                sender.transform = CGAffineTransformMakeRotation(4/5 * rotation)
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0, animations: {
+                sender.transform = CGAffineTransformMakeRotation(5/5 * rotation)
+            })
+        }, completion: nil)
+    }
 }
